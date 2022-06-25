22c5888faeb32979716db117b4dade41bb3d1fc4
HirokiMiyakura <miyakurahiroki@gmail.com>
2022-06-26 01:18:38 +0900

    スポンサー変更

--- templates/sponcor.php
+++ templates/sponcor.php
@@ -1,30 +1,11 @@
 <section>
   <div data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-sine" data-aos-duration="800">
     <h2 class="sectionTitle"><span>SPONCOR</span>スポンサー企業</h2>
-    <!-- <p class="sponcor">
-      <a href="https://studio-peekaboo.jp/" target="_blank">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/peek_a_boo.png" alt="Peek a Boo" />
-      </a>
-     </p> -->
     <div class="sponcor-container">
       <div class="sponcor-container__item">
         <p class="sponcor">
           <a href="https://studio-peekaboo.jp/" target="_blank">
             <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/peek_a_boo.png" alt="Peek a Boo" />
-          </a>
-        </p>
-      </div>
-      <div class="sponcor-container__item">
-        <p class="sponcor">
-          <a href="https://www.54-luck.com/court/kawagoe/" target="_blank">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/five_for_luck.png" alt="five for luck" />
-          </a>
-        </p>
-      </div>
-      <div class="sponcor-container__item">
-        <p class="sponcor">
-          <a href="http://arrowing-llc.jp" target="_blank">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/arrowing.jpg" alt="arrowing" />
           </a>
         </p>
       </div>
@@ -47,6 +28,34 @@
           <!-- <a href="http://ty-doboku.com" target="_blank"> -->
             <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/tsumugi.jpg" alt="arrowing" />
           <!-- </a> -->
+        </p>
+      </div>
+      <div class="sponcor-container__item">
+        <p class="sponcor">
+          <a href="https://kaitai-estaff.com/company/" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/estaff.jpg" alt="" />
+          </a>
+        </p>
+      </div>
+      <div class="sponcor-container__item">
+        <p class="sponcor">
+          <!-- <a href="" target="_blank"> -->
+            <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/fine_estate.jpg" alt="" />
+          <!-- </a> -->
+        </p>
+      </div>
+      <div class="sponcor-container__item">
+        <p class="sponcor">
+          <a href="https://www.kensetumap.com/company/143474/profile.php" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/yoshida_juuki.jpg" alt="" />
+          </a>
+        </p>
+      </div>
+      <div class="sponcor-container__item">
+        <p class="sponcor">
+          <a href="https://www.sakado-fujimi.ed.jp/" target="_blank">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/sponcor/fujimi.jpg" alt="" />
+          </a>
         </p>
       </div>
     </div>
